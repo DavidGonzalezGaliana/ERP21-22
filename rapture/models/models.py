213c368989335ee9ec705fc8f7bde6cf33e45caa
DavidGonzalezGaliana <davidgonzalezgaliana@gmail.com>
2021-10-07 14:53:49 +0000

    creacion de la clase player

--- rapture/models/models.py
+++ rapture/models/models.py
@@ -3,10 +3,28 @@ from odoo import models, fields, api
 
 
 class rapture(models.Model):
-     _name = 'rapture.rapture'
-     _description = 'rapture.rapture'
+    _name = 'rapture.rapture'
+    _description = 'rapture.rapture'
 
-     name = fields.Char()
+    name = fields.Char()
+    science  = fields.Integer(default=100)
+
+
+class player(models.Model):
+    _name = 'rapture.player'
+    _description = 'rapture.player'
+
+    playerName = fields.Char(default="Andrew Ryan")
+    birthplace = fields.Char(default="Russian Empire")
+    gender = fields.Char(default="Male")
+    year_of_birth = fields.Integer(default=1911)
+
+    life = fields.Integer(default=100)
+    hunger = fields.Integer(default=100)
+    sanity = fields.Integer(default=100)
+    oxygen_consumption = fields.Integer(default=0)
+
+
 #     value = fields.Integer()
 #     value2 = fields.Float(compute="_value_pc", store=True)
 #     description = fields.Text()
@@ -15,9 +33,3 @@ class rapture(models.Model):
 #     def _value_pc(self):
 #         for record in self:
 #             record.value2 = float(record.value) / 100
-
-
-
-
-
-
